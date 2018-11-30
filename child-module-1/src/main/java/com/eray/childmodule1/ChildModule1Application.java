f6898d2bcf3112be3f7b357dd5031563c1f884a9
eraykalkan <eray.kalkan@bilgeadam.com>
2018-11-30 11:29:06 +0300

    initial commit

---
+++ child-module-1/src/main/java/com/eray/childmodule1/ChildModule1Application.java
@@ -0,0 +1,12 @@
+package com.eray.childmodule1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildModule1Application.class, args);
+	}
+}
